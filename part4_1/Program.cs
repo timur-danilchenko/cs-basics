0302ad07b3a35698020f276a854253268c002bdd
Tim <timur.danilchenko01@gmail.com>
2023-09-30 16:42:35 +0300

    Homework 4

---
+++ part4_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part4_1
+{
+    internal class Program
+    {
+        public static string[] SplitString(string str)
+        {
+            return str.Split(' ');
+        }
+
+        public static void PrintStrings(string[] strs)
+        {
+            foreach (var s in strs)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            string str = Console.ReadLine();
+            PrintStrings(SplitString(str));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ part4_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part4_2
+{
+    internal class Program
+    {
+        public static string[] SplitString(string str)
+        {
+            return str.Split(' ');
+        }
+
+        public static string ReverseStrings(string[] strs)
+        {
+            string result = "";
+            foreach (var str in strs)
+            {
+                result = str + " " + result;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the string: ");
+            Console.WriteLine("Reversed string: {0}", ReverseStrings(SplitString(Console.ReadLine())));
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+            
+        }
+    }
+}
